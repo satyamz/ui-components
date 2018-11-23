40296339ba9d8f5dd96ab2fde883903ef380a7cc
Akash Srivastava <akashsrivastava4927@gmail.com>
2018-11-23 19:52:32 +0530

    Fix tag shapes issue (#2)
    Signed-off-by: Akash Srivastava <akashsrivastava4927@gmail.com>

--- src/components/GraphNode/tags/_TagFailed.js
+++ src/components/GraphNode/tags/_TagFailed.js
@@ -4,9 +4,9 @@ export default class TagFailed extends React.Component {
   render() {
     return (
       <g transform="translate(3, -9) scale(0.75)">
-        <ellipse stroke="#000000" ry="13.75006" rx="14.50005" id="svg_4" cy="34.25006" cx="31.50005" stroke-width="1.5" fill="#000000"/>
-        <rect stroke="#000000" transform="matrix(0.08671738006422355,-0.0899542209303263,0.09613118731012295,0.08114530344487703,7.3962654910418575,35.03777897880107) " id="svg_6" height="182.69716" width="33" y="43.19679" x="112.89225" stroke-opacity="null" stroke-width="0" fill="#ffffff"/>
-        <rect stroke="#000000" transform="matrix(0.09499618408734749,0.08230740037307374,-0.0879592757342836,0.08889214801190072,31.22523457462846,11.592780964221957) " id="svg_7" height="179.20266" width="33" y="45.7737" x="114.59487" stroke-opacity="null" stroke-width="0" fill="#ffffff"/>
+        <ellipse stroke="#000000" ry="13.75006" rx="14.50005" id="svg_4" cy="34.25006" cx="31.50005" strokeWidth="1.5" fill="#000000"/>
+        <rect stroke="#000000" transform="matrix(0.08671738006422355,-0.0899542209303263,0.09613118731012295,0.08114530344487703,7.3962654910418575,35.03777897880107) " id="svg_6" height="182.69716" width="33" y="43.19679" x="112.89225" strokeOpacity="null" strokeWidth="0" fill="#ffffff"/>
+        <rect stroke="#000000" transform="matrix(0.09499618408734749,0.08230740037307374,-0.0879592757342836,0.08889214801190072,31.22523457462846,11.592780964221957) " id="svg_7" height="179.20266" width="33" y="45.7737" x="114.59487" strokeOpacity="null" strokeWidth="0" fill="#ffffff"/>
       </g>
     );
   }
